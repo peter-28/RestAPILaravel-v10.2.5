f19e7fcf3a6f3936e41e9861f5371a6cddb452c1
unknown <peterlase28@gmail.com>
2025-07-27 22:12:24 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    public $timestamps = true;
+    public $incrementing = true;
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class, 'user_id', 'id');
+    }
+}
